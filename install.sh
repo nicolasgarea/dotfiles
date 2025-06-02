a33096ccf99f747f5c49b3de0b86de80e9a61075
Nicolas Garea <177461094+nicolasgarea@users.noreply.github.com>
2025-06-02 21:44:23 +0200

    feat: add install script to setup zsh, oh-my-zsh, plugins and powerlevel10k

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt update
+sudo apt install -y zsh curl git fonts-powerline
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+cp ./zsh/.zshrc $HOME/.zshrc
+
+echo "Instalación completada. Cambia a zsh con 'chsh -s $(which zsh)' si aún no lo hiciste."
