cef03bf0316fa24213ba11a9ec6e40ad0dd8b7fd
Nicolas Garea <177461094+nicolasgarea@users.noreply.github.com>
2025-06-02 21:50:25 +0200

    feat: update install script to install Alacritty and JetBrainsMono Nerd Font

--- install.sh
+++ install.sh
@@ -22,6 +22,21 @@ if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
 
+if ! command -v alacritty &> /dev/null; then
+  sudo add-apt-repository -y ppa:aslatter/ppa
+  sudo apt update
+  sudo apt install -y alacritty
+fi
+
+
+FONTS_DIR="$HOME/.local/share/fonts"
+mkdir -p "$FONTS_DIR"
+
+if [ ! -f "$FONTS_DIR/JetBrainsMonoNerdFont-Regular.ttf" ]; then
+  wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip | bsdtar -xvf- -C "$FONTS_DIR"
+  fc-cache -fv
+fi
+
 cp ./zsh/.zshrc $HOME/.zshrc
 
 echo "Instalación completada. Cambia a zsh con 'chsh -s $(which zsh)' si aún no lo hiciste."
